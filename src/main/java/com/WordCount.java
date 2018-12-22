3692ffa5ad157b893aa7b426944191bec2588326
JOCHIHEON <zhixian030@gmail.com>
2018-12-22 10:48:12 +0900

    ddf

--- src/main/java/com/WordCount.java
+++ src/main/java/com/WordCount.java
@@ -1,58 +1,61 @@
 package com;
 
+import java.io.IOException;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class WordCount {
-	public static class WCMapper extends Mapper<LongWritable, Text, Text LongWritable>(){
-		private final static LongWritable lw = new LongWritable();
-		private Text word = new Text();
 
+	public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+		private final static LongWritable one = new LongWritable(1);
+		private Text word = new Text();
 		@Override
-		public void map(LongWritable key, Text value, Context context) {
-			  String line = value.toString();
-			  StringTokenizer st = new StringTokenizer(line, "\t\r\n\f|,.()<>");
-			  while(st.hasMoreTokens()) {
-				  context.write(word,lw);
-			  }
-		}​
-	}
-	
-	public static class WCReducer extends Reducer<Text, LongWritable, Text LongWritable>(){
-		private LongWritable lw = new LongWritable();
-		
-		protected void reduce(Text key, Iterable<LongWritable> values, Context context) {
-			long sum = 0;
-			for(LongWritable value:values) {
-				sum += value.get();
+		public void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException {
+			String line = value.toString();
+			StringTokenizer tokenizer =	new StringTokenizer(line, "\t\r\n\f|,.()<> ");
+			while (tokenizer.hasMoreTokens()) {
+				word.set(tokenizer.nextToken().toLowerCase());
+				context.write(word, one);
 			}
-			lw.set(sum);
-			context.write(key,lw);
 		}
 	}
-	
-	public static void main(String[] args) throws Exception{
-		Configuratin conf = new Configuration();
-		Job job = Job.getInstance(conf);
+
+	public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+		private LongWritable sumWritable = new LongWritable();
+		@Override
+		protected void reduce(Text key, Iterable<LongWritable> values,Context context)throws IOException, InterruptedException {
+			long sum = 0;
+			for (LongWritable val : values) {
+				sum += val.get();
+			}
+			sumWritable.set(sum);
+			context.write(key, sumWritable);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf,"WordCount");
 		job.setJarByClass(WordCount.class);
-		job.setMapperClass(WCMapper.class);
-		job.setReducerClass(WCReducer.class);
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(LongWritable.class);
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
-		FileInputFormat.addInputPath(job, new Path("word.text"));
-		FileOutputFormat.setOutputPath(job, new Path("word.log"));
+		FileInputFormat.addInputPath(job, new Path("test2.txt"));
+		FileOutputFormat.setOutputPath(job, new Path("log2"));
 		job.waitForCompletion(true);
 	}
+
 }
